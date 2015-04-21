2d9ce73b95f32361ef89803e7cee44b3226ea819
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2015-04-22 00:05:42 +0100

    Build tools from source

--- make.sh
+++ make.sh
@@ -6,4 +6,16 @@ cd jd-cmd
 mvn clean package
 cp jd-cli/target/jd-cli.jar .. 
 cd ..
+cd dex2jar-build
+mvn clean package
+unzip ./dex-tools/target/dex2jar-*.zip
+rm -Rf ../dex2jar
+mv dex2jar-* ../dex2jar
+cd ..
+chmod 755 dex2jar/*
+cd ./apktool-build
+./gradlew
+./gradlew build fatJar
+cp -f ./brut.apktool/apktool-cli/build/libs/apktool-cli.jar ../apktool/apktool.jar
+cd ..
 pwd
